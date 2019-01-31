a0841d539e197f0f1b83a73743e220319e09bf01
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2019-01-31 23:23:12 +0800

    update several things

--- scripts/utils/helpers.py
+++ scripts/utils/helpers.py
@@ -6,5 +6,5 @@ def parse_dates(dates, reset=False):
         if reset:
             dates = [date.replace(month=1, day=1) for date in dates]
         return [date.isoformat(' ').split(' ')[0] for date in dates]
-    except ValueError:
+    except (ValueError, TypeError):
         return dates
